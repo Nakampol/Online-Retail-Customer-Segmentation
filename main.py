13f07670e71f2cb6ba79049887d534eba1baa281
Nakampol <nakampol.1@gmail.com>
2026-01-22 21:06:25 +0000

    revert first try

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #print hello world 
+<<<<<<< HEAD
 print("git pull command")
+=======
+print("Hello World")
+print("Test Test")
+
+#add print 
+print("Text from github")
+
+#add text again from replit
+print("Text from replit")
+>>>>>>> parent of 0cabdb9 (add_twofunction)
