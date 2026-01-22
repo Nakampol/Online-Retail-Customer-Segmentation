fda7b1b1ca17078407528a1a2487df564e146800
Nakampol <nakampol.1@gmail.com>
2026-01-23 02:28:36 +0700

    Delete some code on main.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 #print hello world 
-print("Hello World")
-print("Test Test")
-
-#add print 
-print("Text from github")
-
-#add text again from replit
-print("Text from replit")
-
-#add fuction +2 for git command 
-def add2(num):
-  return num+2
-
-print(add2(2),add2(3))
+print("git pull command")
